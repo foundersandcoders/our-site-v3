06870ece996c267a2ad42c5dbd7c13410bb25ef6
oliverjam <oliverjamesphillips@gmail.com>
2020-01-28 18:39:28 +0000

    Fix 404 page title

--- src/404.11ty.js
+++ src/404.11ty.js
@@ -2,13 +2,12 @@ const html = require("./html");
 
 exports.data = {
   layout: "layouts/document",
+  title: "Page not found",
   permalink: "404.html",
 };
 
 exports.render = () => {
   return html`
-    <h1>Page not found</h1>
-
     <p class="intro">We're sorry, that page doesn't seem to exist.</p>
   `;
 };

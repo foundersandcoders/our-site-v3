d95a6bde7b8ad3c009a3bd476a226f05a027fb0a
oliverjam <oliverjamesphillips@gmail.com>
2020-01-29 15:31:46 +0000

    Don't show anchor links for h1s
    They're at the top of the page anyway

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,7 @@ module.exports = config => {
   });
 
   md.use(markdownItAnchor, {
+    level: 2,
     slugify, // nicer url slugs
     permalink: true, // show link to headings
     permalinkSymbol: `<svg viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M18 8 C18 8 24 2 27 5 30 8 29 12 24 16 19 20 16 21 14 17 M14 24 C14 24 8 30 5 27 2 24 3 20 8 16 13 12 16 11 18 15"></path></svg>`,

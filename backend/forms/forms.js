34836351da89f2842ec8311060002b4688cd3bc1
oliverjam <oliverjamesphillips@gmail.com>
2021-01-04 21:38:39 +0000

    Add application confirmation email template copy

--- backend/forms/forms.js
+++ backend/forms/forms.js
@@ -12,7 +12,15 @@ exports.handler = async function (event) {
     await email({
       to: data.email,
       subject: "Founders and Coders Application",
-      text: "Thanks for your submission",
+      text: `Hi ${data.name}, 
+
+Thank you for submitting an application to the spring 2021 cohort of Founders and Coders. Please remember you have until 23:59 GMT on January 10th to complete all course requirements. We will send out invitation for interviews the week after application closes. Please note we will contact everyone who's applied, so please do not email us before checking your spam folder.
+
+To stay most up to date with all application news, make sure you're in our Slack community. 
+
+Best wishes,
+
+Founders and Coders Team`,
     });
     return {
       statusCode: 200,

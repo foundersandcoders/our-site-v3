2e278e7a3ded0bb13c2c8ec90d630543714b8004
Yvonne Liu <liuyvonne.ca@gmail.com>
2021-05-23 19:08:01 +0100

    update email response

--- backend/forms/forms.js
+++ backend/forms/forms.js
@@ -23,17 +23,23 @@ exports.handler = async function (event) {
     await db(table).create(airtableData);
     const text = `Hi ${airtableData.name},
 
-Thank you for submitting an application to the spring 2021 cohort of Founders and Coders. You can find a copy of your application below. Please remember you have until 23:59 GMT on January 10 to complete all course requirements. We will send out invitations for interviews the week after applications close.
+Thank you for submitting an application to the autumn 2021 cohort of Founders and Coders. You can find a copy of your application below. Please remember you have until 23:59 BST on May 27 to complete all course requirements. We will send out invitations for interviews the week after applications close.
 
-Please note we will contact everyone who's applied, so please do not email us until after January 16 if you haven’t received an email (or before checking your spam folder).
+As a reminder, our programme now includes a part-time pre-apprenticeship programme for 12 weeks, starting on June 14. This means two evening commitments for 12 weeks, with additional time needed to self-study. We will unfortunately not accept you onto the programme if you cannot make this commitment. 
+
+Please note we will contact everyone who's applied, so please do not email us until after May 31 if you haven’t received an email (or before checking your spam folder).
+
+The interview dates will be June 2-4. 
 
 If you need to edit your application, please email admissions@foundersandcoders.com with a copy of your application.
 
-To stay most up to date with our communication with applicants, make sure to check our Slack community.
+To stay most up to date with our communication with applicants, make sure you’ve joined our Discord community. 
 
 Best wishes,
 
 Founders and Coders Team
+
+
 
 ${emailData}`;
     await email({
